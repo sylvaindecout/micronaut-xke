71c800e6400ac33df49defd03c8aa68a33db7201
Sylvain Decout <sdecout@xebia.fr>
2019-07-24 18:00:03 +0200

    Create stock-update-simulator module

---
+++ random-stock-update-simulator/src/main/java/fr/xebia/xke/micronaut/booking/simulator/Application.java
@@ -0,0 +1,10 @@
+package fr.xebia.xke.micronaut.booking.simulator;
+
+import io.micronaut.runtime.Micronaut;
+
+public class Application {
+
+    public static void main(final String[] args) {
+        Micronaut.run(Application.class);
+    }
+}
