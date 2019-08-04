a3030cdb84b3ae07dcc01736e1a3dc31b828298f
Sylvain Decout <sdecout@xebia.fr>
2019-08-04 18:27:03 +0200

    Pricing service: Create module

---
+++ pricing-service/src/main/java/fr/xebia/xke/micronaut/pricing/Application.java
@@ -0,0 +1,9 @@
+package fr.xebia.xke.micronaut.pricing;
+
+import io.micronaut.runtime.Micronaut;
+
+public class Application {
+    public static void main(final String[] args) {
+        Micronaut.run(Application.class);
+    }
+}
