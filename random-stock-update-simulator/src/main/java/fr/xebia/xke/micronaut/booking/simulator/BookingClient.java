26e446aa24ecaadc3c28bf9b7a99c3afcc7dbf26
Sylvain Decout <sdecout@xebia.fr>
2019-08-04 09:12:47 +0200

    Add circuit breaker

--- random-stock-update-simulator/src/main/java/fr/xebia/xke/micronaut/booking/simulator/BookingClient.java
+++ random-stock-update-simulator/src/main/java/fr/xebia/xke/micronaut/booking/simulator/BookingClient.java
@@ -6,7 +6,9 @@ import io.micronaut.http.annotation.Post;
 import io.micronaut.http.annotation.Put;
 import io.micronaut.http.annotation.QueryValue;
 import io.micronaut.http.client.annotation.Client;
+import io.micronaut.retry.annotation.CircuitBreaker;
 
+@CircuitBreaker(reset = "30s")
 @Client("${booking.base-url}/booking")
 public interface BookingClient {
 
