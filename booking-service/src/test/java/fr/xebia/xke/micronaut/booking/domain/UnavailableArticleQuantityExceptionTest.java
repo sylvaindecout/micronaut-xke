c08d6a1cdd549a3c336d3d73aa023e382b6d6d15
Sylvain Decout <sdecout@xebia.fr>
2019-11-10 21:20:44 +0100

    Booking service: Use property-based testing for exception

--- booking-service/src/test/java/fr/xebia/xke/micronaut/booking/domain/UnavailableArticleQuantityExceptionTest.java
+++ booking-service/src/test/java/fr/xebia/xke/micronaut/booking/domain/UnavailableArticleQuantityExceptionTest.java
@@ -1,39 +1,43 @@
 package fr.xebia.xke.micronaut.booking.domain;
 
 
-import org.junit.jupiter.api.Test;
+import net.jqwik.api.ForAll;
+import net.jqwik.api.Property;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatNullPointerException;
 
 class UnavailableArticleQuantityExceptionTest {
 
-    private static final ArticleReference VALID_ARTICLE = new ArticleReference("BOOK00001");
-    private static final Quantity VALID_QUANTITY = new Quantity(13L);
-
-    @Test
-    void should_initialize_exception_with_explicit_message() {
-        final Exception exception = new UnavailableArticleQuantityException(new ArticleReference("BOOK00001"), new Quantity(12L), new Quantity(2L));
+    @Property
+    void should_initialize_exception_with_explicit_message(@ForAll ArticleReference requestedArticle,
+                                                           @ForAll Quantity requestedQuantity,
+                                                           @ForAll Quantity availableQuantity) {
+        final Exception exception = new UnavailableArticleQuantityException(requestedArticle, requestedQuantity, availableQuantity);
         assertThat(exception.getMessage())
-                .isEqualTo("Requested quantity (%s) is unavailable for article '%s' (available: %s)", 12, "BOOK00001", 2);
+                .isEqualTo("Requested quantity (%s) is unavailable for article '%s' (available: %s)",
+                        requestedQuantity, requestedArticle, availableQuantity);
     }
 
-    @Test
-    void should_fail_to_initialize_exception_if_requested_article_is_missing() {
+    @Property
+    void should_fail_to_initialize_exception_if_requested_article_is_missing(@ForAll Quantity requestedQuantity,
+                                                                             @ForAll Quantity availableQuantity) {
         assertThatNullPointerException().isThrownBy(() ->
-                new UnavailableArticleQuantityException(null, VALID_QUANTITY, VALID_QUANTITY));
+                new UnavailableArticleQuantityException(null, requestedQuantity, availableQuantity));
     }
 
-    @Test
-    void should_fail_to_initialize_exception_if_requested_quantity_is_missing() {
+    @Property
+    void should_fail_to_initialize_exception_if_requested_quantity_is_missing(@ForAll ArticleReference requestedArticle,
+                                                                              @ForAll Quantity availableQuantity) {
         assertThatNullPointerException().isThrownBy(() ->
-                new UnavailableArticleQuantityException(VALID_ARTICLE, null, VALID_QUANTITY));
+                new UnavailableArticleQuantityException(requestedArticle, null, availableQuantity));
     }
 
-    @Test
-    void should_fail_to_initialize_exception_if_available_quantity_is_missing() {
+    @Property
+    void should_fail_to_initialize_exception_if_available_quantity_is_missing(@ForAll ArticleReference requestedArticle,
+                                                                              @ForAll Quantity requestedQuantity) {
         assertThatNullPointerException().isThrownBy(() ->
-                new UnavailableArticleQuantityException(VALID_ARTICLE, VALID_QUANTITY, null));
+                new UnavailableArticleQuantityException(requestedArticle, requestedQuantity, null));
     }
 
 }
