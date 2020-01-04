39b1a1763660068df2086d56dd48ad4cd3432d56
Sylvain Decout <sdecout@xebia.fr>
2020-01-04 15:28:21 +0100

    Add test instructions to README

---
+++ catalogue-service/setup.sh
@@ -0,0 +1,4 @@
+curl -X PUT http://localhost:8087/catalogue/articles/BookNumber1?priceAsCents=1499
+curl -X PUT http://localhost:8087/catalogue/articles/BookNumber2?priceAsCents=2950
+curl -X PUT http://localhost:8087/catalogue/articles/BookNumber3?priceAsCents=1299
+curl -X PUT http://localhost:8087/catalogue/articles/MagicianHat?priceAsCents=10000
